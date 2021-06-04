df068a389910e14b22a8639e9da82994db344f56
duk <cyqt1004@gmail.com>
2021-06-04 16:07:19 +0900

    new

--- src/main/java/com/example/people/controller/BoardController.java
+++ src/main/java/com/example/people/controller/BoardController.java
@@ -1,8 +1,28 @@
 package com.example.people.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.people.model.BoardEntity;
+import com.example.people.repository.BoardRepository;
+
+@RestController
 public class BoardController {
-
+	
+	@Autowired
+	public BoardRepository board;
+	
+	public BoardController(BoardRepository repo) {
+		this.board = repo;
+	}
+	@GetMapping(value = "/board")
+	public List<BoardEntity> getBoard() {
+		List<BoardEntity> list = board.findAll();
+		return list;
+	}
+	
+	
 }
